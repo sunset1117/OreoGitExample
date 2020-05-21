048deff4b8b78970ba3f0b6560f2129b3f3bf96f
sunset1117 <sunset1117@hotmail.com>
2020-05-21 19:19:51 +0800

    first commit

---
+++ GitTest/src/utils/ChangeDateFormate.java
@@ -0,0 +1,52 @@
+package utils;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public interface ChangeDateFormate {
+
+////把yyyy-MM-dd格式的字串轉成sql.Date
+	public static java.sql.Date ChangeDateFormate_Dash(String yyyyMMdd) {
+		DateFormat formate = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			java.sql.Date sqlDate = new java.sql.Date(formate.parse(yyyyMMdd).getTime());
+			return sqlDate;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new java.sql.Date(0);
+	}
+
+////把yyyy/MM/dd格式的字串轉成sql.Date
+	public static java.sql.Date ChangeDateFormate_Slash(String yyyyMMdd) {
+		DateFormat formate = new SimpleDateFormat("yyyy/MM/dd");
+		try {
+			java.sql.Date sqlDate = new java.sql.Date(formate.parse(yyyyMMdd).getTime());
+			return sqlDate;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new java.sql.Date(0);
+	}
+
+////把yyyyMMdd格式的字串轉成sql.Date
+	public static java.sql.Date ChangeDateFormate(String yyyyMMdd) {
+		DateFormat formate = new SimpleDateFormat("yyyyMMdd");
+		try {
+			java.sql.Date sqlDate = new java.sql.Date(formate.parse(yyyyMMdd).getTime());
+			return sqlDate;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new java.sql.Date(0);
+	}
+
+////測試上面的幾個方法
+	public static void main(String[] args) {
+		System.out.println(ChangeDateFormate_Dash("2020-02-31"));
+		System.out.println(ChangeDateFormate_Slash("2020/02/31"));
+		System.out.println(ChangeDateFormate("20200231"));
+		System.out.println(new Timestamp(System.currentTimeMillis()));
+	}
+}
